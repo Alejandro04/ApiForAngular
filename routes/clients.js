6e646532181a192a7dd837b34b996285c9aa3039
Alejandro Roa <a.roa@fixme.es>
2019-10-23 11:29:45 -0500

    init

---
+++ routes/clients.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+
+const Client = require('../models/Client')
+
+router.get('/clients', async (req, res) => {
+    const clients = await Client.find()
+
+    res.json(clients);
+})
+
+router.post('/clients', async (req, res) => {
+    const { name, description } = req.body;
+    const errors = [];
+    if (!name) {
+        res.json({
+            message: 'Ingrese el nombre'
+        })
+    }
+    if (!description) {
+        res.json({
+            message: 'Ingrese la descripción'
+        })
+    }
+    if (errors.length > 0) {
+        res.json({
+            errors,
+            name,
+            description
+        })
+    } else {
+        try {
+            const newClient = new Client({ name, description });
+            await newClient.save();
+
+            res.json({
+                client: newClient
+            });
+        } catch (err) {
+            console.log(err)
+        }
+    }
+})
+
+
+router.get('/clients/:id', async (req, res) => {
+    const client = await Client.findById(req.params.id)
+    res.json(client);
+})
+
+/*
+router.put('/clients/:id', async (req, res) => {
+  const { name, description } = req.body;
+  await Client.findByIdAndUpdate(req.params.id, {title, description});
+  
+  return "Cliente editado"
+});
+
+router.delete('/notes/:id',  async (req, res) => {
+  await Client.findByIdAndDelete(req.params.id);
+  
+  return "Cliente Eliminado"
+});
+*/
+
+module.exports = router;
